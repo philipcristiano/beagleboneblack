0dfa08301d22b8cda6e3813ff3c87b9eb080a815
Philip Cristiano <philipcristiano@gmail.com>
2000-01-01 01:32:27 +0000

    datalog: Save temperature to librato

--- datalog.py
+++ datalog.py
@@ -1,22 +1,29 @@
 import random
 import time
 import os
+import sys
 
 import librato
 from luxmeter import Luxmeter
+from mcp9808 import MCP9808
 
 lib_api = librato.connect(os.getenv('LIBRATO_USER'), os.getenv('LIBRATO_TOKEN'))
 
 def main():
 	last_time = 0
 	lm = Luxmeter()
+	mcp = MCP9808()
 	while True:
 		lux = lm.getLux(1)
 		print 'Lux', lux
+		temp_f = mcp.read_temp_f()
+		print 'Temp', temp_f
 		if time.time() > last_time + 60:
 			lib_api.submit('home_lux', lux)
+			lib_api.submit('home_temp_f', temp_f)
 			last_time = time.time()
 			print 'Saved to Librato'
+		sys.stdout.flush()
 		time.sleep(1)
 
 if __name__ == '__main__':
