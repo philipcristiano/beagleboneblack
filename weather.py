1c8fe23dc956ba94582c168eaf9b1989c3390251
Philip Cristiano <philipcristiano@gmail.com>
2014-04-15 23:58:48 +0000

    Working weather script

--- weather.py
+++ weather.py
@@ -1,6 +1,8 @@
 import httplib
-import urllib2
 import json
+import time
+import urllib2
+
 from Adafruit_7Segment import SevenSegment
 
 segment = SevenSegment(address=0x70)
@@ -8,12 +10,15 @@ segment = SevenSegment(address=0x70)
 def main():
     while True:
         temp = get_temp()
+        display_temp(temp)
+        time.sleep(180)
 
 def display_temp(temp):
-    segment.writeDigit(0, int(temp / 100))
+    #segment.writeDigit(0, int(temp / 100))
     segment.writeDigit(1, int(temp / 10))
-    segment.writeDigit(2, int(temp % 10), dot=True)
-    segment.writeDigit(3, int((temp *10) % 10))
+    segment.writeDigit(3, int(temp % 10), dot=True)
+    segment.writeDigit(4, int((temp * 10) % 10))
+    segment.setColon(False)
 
 def get_temp():
     url = 'http://api.openweathermap.org/data/2.5/weather?q=new+york,ny,us'
